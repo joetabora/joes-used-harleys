0caaf58c495d813978316e16c25338c8f0780920
Joseph Tabora <joetabora@gmail.com>
2025-12-11 19:27:09 -0600

    Replace bannerjoe.jpeg with juh4.jpg banner

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -262,7 +262,7 @@ export function Navigation() {
         background: '#000000'
       }}>
         <Image
-          src="/bannerjoe.jpeg"
+          src="/juh4.jpg"
           alt="Joe's Used Harleys"
           width={1920}
           height={300}
